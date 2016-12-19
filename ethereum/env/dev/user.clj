0ee4d9885f95a88ccf38dfa3f52922c54081fb3d
Alvaro Castro-Castilla <alvcastro@yahoo.es>
2016-12-19 17:48:42 +0400

    project improvements

--- ethereum/env/dev/user.clj
+++ ethereum/env/dev/user.clj
@@ -1,7 +1,10 @@
 (ns user
-  (:require [figwheel-sidecar.repl-api]
-            [ethereum.core]
-            [ring.middleware.reload :refer [wrap-reload]]))
+  (:require [figwheel-sidecar.repl-api]))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
+
+(defn run []
+  (figwheel-sidecar.repl-api/start-figwheel! (figwheel-sidecar.config/fetch-config))
+  (figwheel-sidecar.repl-api/cljs-repl)
+  (in-ns 'ethereum.core))
--- ethereum/project.clj
+++ ethereum/project.clj
@@ -1,39 +1,39 @@
-(defproject figwheel4node "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.107"]]
+(defproject ethereum "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]]
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-figwheel "0.5.8"]]
   :clean-targets ^{:protect false} ["target"]
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "0.8.2"]
+   {:dependencies [[binaryage/devtools "0.8.3"]
                    [com.cemerick/piggieback "0.2.1"]
                    [figwheel-sidecar "0.5.8"]
-                   [org.clojure/tools.nrepl "0.2.11"]]
-    :plugins [[lein-figwheel "0.5.8"]]
+                   [org.clojure/tools.nrepl "0.2.12"]]
+    :plugins [[lein-figwheel "0.5.8"]
+              [lein-ancient "0.6.10"]]
     :source-paths ["env/dev"]
     :cljsbuild {:builds [{:id "dev"
                           :source-paths ["src/ethereum"]
                           :figwheel {:on-jsload "ethereum.core/mount-root"}
                           :compiler {:main ethereum.core
-                                     :output-to "target/server_out/ethereum_with_figwheel.js"
-                                     :output-dir "target/server_out"
+                                     :output-to "target/dev/ethereum.js"
+                                     :output-dir "target/dev/"
                                      :target :nodejs
                                      :optimizations :none
                                      :source-map-timestamp true
                                      :source-map true}}]}}
-
-   ;; :uberjar {:hooks [leiningen.cljsbuild]
-   ;;               :omit-source true
-   ;;               :aot :all
-   ;;               :main emojillionaire.core
-   ;;               :cljsbuild {:builds {:app {:id "uberjar"
-   ;;                                          :source-paths ["src/cljs"]
-   ;;                                          :compiler {:main clojurescript-ethereum-example.core
-   ;;                                                     :output-to "resources/public/js/compiled/app.js"
-   ;;                                                     :optimizations :advanced
-   ;;                                                     :closure-defines {goog.DEBUG false}
-   ;;                                                     :pretty-print true
-   ;;                                                     :pseudo-names true}}}}}
-   }
+   :deploy {:hooks [leiningen.cljsbuild]
+            :omit-source true
+            :aot :all
+            :main ethereum.core
+            :cljsbuild {:builds {:app {:id "uberjar"
+                                       :source-paths ["src/deploy/"]
+                                       :compiler {:main ethereum.core
+                                                  :output-to "target/deploy/ethereum.js"
+                                                  ;; Try advanced, but see https://github.com/bhauman/lein-figwheel/wiki/Node.js-development-with-figwheel
+                                                  :optimizations :simple
+                                                  :closure-defines {goog.DEBUG false}
+                                                  :pretty-print true
+                                                  :pseudo-names true}}}}}}
   :figwheel {})
