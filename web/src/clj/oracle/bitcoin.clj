02cef0cd6928dde2353f011c763194a9231a161d
Alvaro Castro-Castilla <alvcastro@yahoo.es>
2016-12-29 21:50:59 +0100

    Multisig, finally

--- web/src/clj/oracle/bitcoin.clj
+++ web/src/clj/oracle/bitcoin.clj
@@ -2,7 +2,8 @@
   (:require [environ.core :refer [env]]
             [clojure.pprint :refer [pprint]])
   (:import [java.io File]
-           (java.net InetAddress)
+           [java.net InetAddress]
+           [java.util ArrayList]
            [com.google.common.collect ImmutableList]
            [org.bitcoinj.core Address BlockChain Coin Context ECKey PeerAddress PeerGroup Transaction
             TransactionBroadcast]
@@ -10,7 +11,7 @@
            [org.bitcoinj.kits WalletAppKit]
            [org.bitcoinj.net.discovery DnsDiscovery]
            [org.bitcoinj.params MainNetParams TestNet3Params RegTestParams]
-           [org.bitcoinj.script ScriptBuilder]
+           [org.bitcoinj.script Script ScriptBuilder]
            [org.bitcoinj.store MemoryBlockStore SPVBlockStore]
            [org.bitcoinj.utils BriefLogFormatter]
            [org.bitcoinj.wallet SendRequest Wallet]))
@@ -31,6 +32,10 @@
 
 (defn substract-btc-fee [sat]
   (- sat 0.00005))
+
+(defn make-tx-broadcast-progress-cb []
+  (reify org.bitcoinj.core.TransactionBroadcast$ProgressCallback
+    (onBroadcastProgress [this p] (println p))))
 
 ;;
 ;; App
@@ -106,93 +111,85 @@
 ;; Multisig
 ;;
 
-(defrecord MultiSig [our-key seller-key buyer-key contract script])
-
 ;; (. ECKey fromPrivate (BigInteger. "103169733757778458218722489788847239787967310021819641785899608061388154372397"))
 ;; (. org.bitcoinj.core.Base58 encode (.getSecretBytes (. ECKey fromPrivate (BigInteger. "103169733757778458218722489788847239787967310021819641785899608061388154372397"))))
 ;; Private key: "GMPBVAgMHJ6jUk7g7zoYhQep5EgpLAFbrJ4BbfxCr9pp"
 ;; Address: "mjd3Ug6t53rbsrZhZpCdobqHDUnU8sjAYd"
 
-(defn create-multisig [app]
+(defrecord MultiSig [our-key seller-key buyer-key tx script])
+
+(defn create-multisig [app wallet]
   (let [our-key (ECKey.)
-        ;;our-key (. wallet currentReceiveKey)
+        seller-key (ECKey.)
+        buyer-key (ECKey.)
+        keys (. ImmutableList of our-key seller-key buyer-key)
+        script (. ScriptBuilder createMultiSigOutputScript 2 keys)
+        tx (Transaction. (:network-params app))
+        multisig (MultiSig. our-key seller-key buyer-key tx script)
+        _ (.addOutput tx (. Coin valueOf 1 0) script)
+        request (. SendRequest forTx tx)]
+    (.completeTx wallet request)
+    (.broadcastTransaction (:peergroup app) (.tx request))
+    multisig))
+
+(defn multisig-spend [multisig app input-tx target-address key1 key2 & [key3]]
+  (let [multisig-out (.getOutput input-tx 0)
+        value (.getValue multisig-out)
+        ;; Signature 1
+        tx1 (Transaction. (:network-params app))
+        _ (.addOutput tx1 value (. Address fromBase58 (:network-params app) target-address))
+        _ (.addInput tx1 multisig-out)
+        signature1 (.calculateSignature tx1
+                                        0
+                                        key1
+                                        (:script multisig)
+                                        org.bitcoinj.core.Transaction$SigHash/ALL
+                                        false)
+        ;; Signature 2
+        tx2 (Transaction. (:network-params app))
+        _ (.addOutput tx2 value (. Address fromBase58 (:network-params app) target-address))
+        input (.addInput tx2 multisig-out)
+        signature2 (.calculateSignature tx2
+                                        0
+                                        key2
+                                        (:script multisig)
+                                        org.bitcoinj.core.Transaction$SigHash/ALL
+                                        false)
+        input-script (. ScriptBuilder createMultiSigInputScript [signature1 signature2])]
+    (.setScriptSig input input-script)
+    (.verify input multisig-out)
+    (.broadcastTransaction (:peergroup app) tx2)))
+
+;;
+;; P2SH Multisig
+;;
+
+(defrecord P2SHMultiSig [our-key seller-key buyer-key p2sh-script redeem-script])
+
+(defn create-p2hs-multisig [app]
+  (let [our-key (ECKey.)
         ;;  We can get the other parties public key from somewhere
         ;; ECKey serverKey = new ECKey(null, publicKeyBytes);
         seller-key (ECKey.)
         buyer-key (ECKey.)
         keys (. ImmutableList of our-key seller-key buyer-key)
-        ;; Prepare a template for the contract.
-        contract (Transaction. (:network-params app))
-        script (. ScriptBuilder createMultiSigOutputScript 2 keys)]
-    (MultiSig. our-key seller-key buyer-key contract script)))
+        redeem-script (. ScriptBuilder createRedeemScript 2 keys)
+        p2sh-script (. ScriptBuilder createP2SHOutputScript redeem-script)]
+    (P2SHMultiSig. our-key seller-key buyer-key p2sh-script redeem-script)))
 
-;; (defn multisig-get-our-address [multisig network-params]
-;;   (.toString (.toAddress (:our-key multisig) network-params)))
-
-(defn multisig-setup-from-wallet [multisig wallet app]
-  (let [wallet-tx (first (.getUnspents wallet))
-        coins (.getBalance wallet) ;;(. Coin valueOf 0 0001)
-        output (.addOutput (:contract multisig) coins (:script multisig))
-        input (.addInput (:contract multisig) wallet-tx)]
-    (let [request (. SendRequest forTx (:contract multisig))]
-      (try (.completeTx wallet request)
-           ;;(. (:peergroup app) broadcastTransaction (.tx request))
-           (.broadcastTransaction (:peergroup app) (.tx request))
-           (catch Exception e (println e))))))
-
-(defn multisig-send-ours [multisig app target-address]
-  (let [multisig-output (.getOutput (:contract multisig) 0)
-        ;;multisig-script (.getScriptPubKey transaction-output)
-        value (.getValue multisig-output)]
-    ;;(.isSentToMultiSig (:script multisig))
-    (let [spend-tx (Transaction. (:network-params app))]
-      (.addOutput spend-tx value (. Address fromBase58 (:network-params app) target-address))
-      (.addInput spend-tx multisig-output)
-      (.calculateSignature spend-tx
-                           0
-                           (:our-key multisig)
-                           (:script multisig)
-                           (org.bitcoinj.core.Transaction$SigHash/ALL)
-                           false))))
-
-(defn multisig-send-other [multisig app target-address our-signature other-key]
-  (let [multisig-output (.getOutput (:contract multisig) 0)
-        value (.getValue multisig-output)
-        spend-tx (Transaction. (:network-params app))]
-    (.addOutput spend-tx value (. Address fromBase58 (:network-params app) target-address))
-    (let [input (.addInput spend-tx multisig-output)
-          other-signature (.calculateSignature spend-tx
-                                               0
-                                               other-key
-                                               (:script multisig)
-                                               (org.bitcoinj.core.Transaction$SigHash/ALL)
-                                               false)
-          spend-script (. ScriptBuilder
-                          createMultiSigInputScript
-                          (. ImmutableList of other-signature our-signature))]
-      (.setScriptSig input spend-script)
-      (.verify input multisig-output)
-      (.broadcastTransaction (:peergroup app) spend-tx)
-      [spend-tx spend-script])))
+(defn p2hs-multisig-address [multisig app]
+  (. Address fromP2SHHash (:network-params app) (.getPubKeyHash (:p2sh-script multisig))))
 
 (comment
   (def app (make-app))
   (app-start! app)
   (def w (make-wallet app))
   (def app (app-add-wallet app w))
-  (def multisig (create-multisig app))
-  ;;(def multisig-address (multisig-get-our-address multisig app))
-  (def mtx (multisig-setup-from-wallet multisig w app))
-  ;;(wallet-send-coins (get-current-wallet) (get-current-peergroup) multisig-address 10000)
-  #_(multisig-send-other multisig
-                       (get-current-peergroup)
-                       (get-current-network-params)
-                       "n2eMqTT929pb1RDNuqEnxdaLau1rxy3efi"
-                       txsig
-                       (:seller-key multisig)))
-
-(def tx-broadcast-progress-cb
-  (reify org.bitcoinj.core.TransactionBroadcast$ProgressCallback
-    (onBroadcastProgress [this p] (println p))))
-;;
-
+  (def multisig (create-multisig app w))
+  (multisig-spend multisig
+                  app
+                  (first (.getTransactionsByTime w))
+                  (.toString (wallet-get-current-address w))
+                  (:our-key multisig)
+                  (:seller-key multisig))
+  (def mtx (multisig-setup-from-wallet multisig w app)))
